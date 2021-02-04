980463601d00f8a29609c5adb81a115926f5f07a
JanosSarkoezi <j.sarkoezi@googlemail.com>
2021-02-04 17:04:42 +0100

    simple timer

--- zen/timer/src/timer.c
+++ zen/timer/src/timer.c
@@ -5,54 +5,31 @@
 #include <stdlib.h>
 
 void childProcess(int argc, char** argv, char** envp);
-void parentProcess(int argc, char** argv, int* childStatus);
-int callWithFork(int argc, char** argv, char** envp, int* childStatus);
+void parentProcess(int argc, char** argv);
+int callWithFork(int argc, char** argv, char** envp);
 
 int main(int argc, char** argv, char **envp) {
-  
-    for (char **p = argv; *p != 0; ++p) {
-        printf("%s\n", *p);
-    }
-
-    // printf("Starting sleep ...");
-    // fflush(stdout);
-    // sleep(atoi(*argv++));
-    // // ++argv;
-    // printf("call execve ...");
-    // execve(argv[0], argv, envp);
+    callWithFork(argc, argv, envp);
 }
 
-int callWithFork(int argc, char** argv, char** envp, int* childStatus) {
-    // for (char **env = envp; *env != 0; env++) {
-    //     char *thisEnv = *env;
-    //     printf("%s\n", thisEnv);
-    // }
-
+int callWithFork(int argc, char** argv, char** envp) {
     if (fork() == 0) {
         childProcess(argc, argv, envp);
     } else {
-        parentProcess(argc, argv, childStatus);
+        parentProcess(argc, argv);
     }
 
-    printf("Bye\n");
     return 0;
 }
 
 void childProcess(int argc, char** argv, char** envp) {
-    printf("HC: hello from child\n");
-    fflush(stdout);
-    sleep(10);
+    char* const execParams[] = {"asdf", "Nachricht", "Die Zeit ist abgelaufen", NULL};
+
     ++argv;
-    execve(argv[0], argv, envp);
+
+    sleep(atoi(*argv));
+    execv("/usr/bin/notify-send", execParams);
 }
 
-void parentProcess(int argc, char** argv, int *childStatus) {
-    printf("HP: hello from parent\n");
-    fflush(stdout);
-
-    wait(childStatus);
-
-    printf("CT: child has terminated\n");
-    fflush(stdout);
-
+void parentProcess(int argc, char** argv) {
 }
